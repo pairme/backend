925bfd8f2ab8a607ec81cbf1e845e6287fe4ac73
CarloC24 <carlo.clamucha@gmail.com>
2019-07-04 14:49:02 -0500

    adding admin functionality

--- index.js
+++ index.js
@@ -20,6 +20,7 @@ const port = process.env.PORT || 5000;
   */
 let connections = [];
 let whosTyping = [];
+let admins = [];
 /*
 init our needed stuff
 logic will get refactored later but for now
@@ -90,6 +91,17 @@ io.on("connection", function(socket) {
     console.log(`socket connected! sockets remaining : ${connections.length}`);
   });
 
+  socket.on("add admin", function(data) {
+    socket.chat_name = "Staff: " + data;
+    socket.pairme_admin = true;
+    admins.push(socket);
+    io.emit("admins connected", admins.length);
+    io.emit("message", {
+      message: `${socket.chat_name} has entered the chat`,
+      id: Math.random()
+    });
+  });
+
   /*
   adding typing functionality
   */
@@ -115,13 +127,21 @@ io.on("connection", function(socket) {
       message: `${socket.chat_name} left the chat`,
       id: Math.random()
     });
-    const newConnections = connections.filter(
-      connection => connection != socket
-    );
-    // update the connections array with our new connections array!
-    connections = newConnections;
-    io.emit("connections count", connections.length);
-    console.log(`socket disconected sockets remaining : ${connections.length}`);
+    if (socket.pairme_admin) {
+      const newAdmins = admins.filter(connection => connection != socket);
+      // update the connections array with our new connections array!
+      admins = newAdmins;
+    } else {
+      const newConnections = connections.filter(
+        connection => connection != socket
+      );
+      // update the connections array with our new connections array!
+      connections = newConnections;
+      io.emit("connections count", connections.length);
+      console.log(
+        `socket disconected sockets remaining : ${connections.length}`
+      );
+    }
     // disconnect the room!!!
   });
   socket.on("message", msg => {
